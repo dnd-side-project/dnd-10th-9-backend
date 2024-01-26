a313b9a24586f9995064bd90f56b8fb48f979f46
jihwan <jihwan1535@nate.com>
2024-01-26 21:27:32 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/dnd/nine/NineApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.nine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NineApplication.class, args);
+	}
+
+}
