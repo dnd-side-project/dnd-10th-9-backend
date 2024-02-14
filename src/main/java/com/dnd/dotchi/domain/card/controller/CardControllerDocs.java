3701b95ff7667869ad7696fea6958a154e7a375f
tjdtls690 <tjdtls690@gmail.com>
2024-02-15 03:33:56 +0900

    feat : (#20) 댓글 작성 기능 Swagger 작성

--- src/main/java/com/dnd/dotchi/domain/card/controller/CardControllerDocs.java
+++ src/main/java/com/dnd/dotchi/domain/card/controller/CardControllerDocs.java
@@ -2,6 +2,7 @@ package com.dnd.dotchi.domain.card.controller;
 
 import com.dnd.dotchi.domain.card.dto.request.CardsByThemeRequest;
 import com.dnd.dotchi.domain.card.dto.response.CardsByThemeResponse;
+import com.dnd.dotchi.domain.card.dto.response.WriteCommentOnCardResponse;
 import com.dnd.dotchi.global.exception.ExceptionResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Tag(name = "카드", description = "카드 API")
 public interface CardControllerDocs {
@@ -32,5 +34,17 @@ public interface CardControllerDocs {
             content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
     )
     ResponseEntity<CardsByThemeResponse> getCardsByTheme(final CardsByThemeRequest request);
+
+    @Operation(summary = "댓글 작성", description = "카드에 댓글을 작성한다.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "댓글 작성 성공"
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "존재하지 않는 카드 ID인 경우",
+            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
+    )
+    ResponseEntity<WriteCommentOnCardResponse> writeCommentOnCard(@PathVariable("cardId") Long cardId);
 
 }
