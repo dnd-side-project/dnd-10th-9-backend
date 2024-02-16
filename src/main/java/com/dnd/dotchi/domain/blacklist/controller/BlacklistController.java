dedc17e2dc3aa537100be7bb36d124cd40fae1f6
tjdtls690 <tjdtls690@gmail.com>
2024-02-16 16:53:01 +0900

    feat : (#32) Swagger 명세 작성

--- src/main/java/com/dnd/dotchi/domain/blacklist/controller/BlacklistController.java
+++ src/main/java/com/dnd/dotchi/domain/blacklist/controller/BlacklistController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/blacklists")
 @RequiredArgsConstructor
-public class BlacklistController {
+public class BlacklistController implements BlacklistControllerDocs {
 
     private final BlacklistService blacklistService;
 
