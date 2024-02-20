84fc25300fa0dd6174f5408fdc9236723126eb81
jihwan <jihwan1535@nate.com>
2024-02-20 23:41:04 +0900

    refactor : (#42) 카드 조회 swagger 약간 수정

--- src/main/java/com/dnd/dotchi/domain/card/dto/request/CardsAllRequest.java
+++ src/main/java/com/dnd/dotchi/domain/card/dto/request/CardsAllRequest.java
@@ -11,12 +11,12 @@ public record CardsAllRequest(
         @NotNull(message = "카드 정렬 타입은 빈 값일 수 없습니다.")
         CardSortType cardSortType,
 
-        @Schema(description = "마지막으로 조회한 카드 ID", example = "1")
+        @Schema(description = "마지막으로 조회한 카드 ID", example = "99999")
         @NotNull(message = "마지막으로 조회한 카드 ID는 빈 값일 수 없습니다.")
         @Positive(message = "마지막으로 조회한 카드 ID는 양수만 가능합니다.")
         Long lastCardId,
 
-        @Schema(description = "마지막으로 조회한 카드의 댓글 수", example = "1")
+        @Schema(description = "마지막으로 조회한 카드의 댓글 수", example = "99999")
         @NotNull(message = "마지막으로 조회한 카드의 댓글 수는 빈 값일 수 없습니다.")
         @Positive(message = "마지막으로 조회한 카드의 댓글 수는 양수만 가능합니다.")
         Long lastCardCommentCount
