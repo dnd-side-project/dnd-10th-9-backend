0b7e7df614d1385f61927422d09b45851286fbf7
tjdtls690 <tjdtls690@gmail.com>
2024-02-23 16:45:44 +0900

    feat : (#65) 응답 시 카드 리스트 변수명 개선

--- src/main/java/com/dnd/dotchi/domain/card/dto/response/CardsByThemeResultResponse.java
+++ src/main/java/com/dnd/dotchi/domain/card/dto/response/CardsByThemeResultResponse.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Schema(description = "테마별 카드 조회 결과 응답")
 public record CardsByThemeResultResponse(
         @Schema(description = "해당 테마의 최신순 카드 목록")
-        List<RecentCardsByThemeResponse> recentCards
+        List<RecentCardsByThemeResponse> cards
 ) {
         public static CardsByThemeResultResponse from(final List<Card> cardsByTheme) {
                 return new CardsByThemeResultResponse(parseRecentCardsByThemeResponse(cardsByTheme));
